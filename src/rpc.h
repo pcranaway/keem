b09c92aebc975e0b0abddd747ac3632cdbe125cd
pc <crunch@disroot.org>
2021-10-17 09:31:12 -0400

    add rpc.h (wip)

---
+++ src/rpc.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdlib>
+#include <string>
+
+enum HTTPMethod {
+	POST,
+	PUT,
+	PATCH,
+	DELETE
+};
+
+class RPC {
+
+	public:
+		// RPC(std::string name, protocol::M);
+
+};
