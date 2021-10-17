91a450641e68a210f9fc048725dfcf39c5c44651
pcranaway <pcranaway@tuta.io>
2021-10-17 17:24:40 +0300

    format

--- src/keem.cc
+++ src/keem.cc
@@ -1,45 +1,48 @@
+#include <json.hpp>
+#include <regex>
 #include <stdio.h>
 #include <string>
 #include <workflow/HttpMessage.h>
 #include <workflow/WFHttpServer.h>
-#include <json.hpp>
-#include <regex>
 
 using json = nlohmann::json;
 
 const std::regex kRpcRegex("^(\\/rpc\\/)([A-z]+)$");
 
-void json_response(protocol::HttpResponse *http_resp, json data, int status_code) {
-	http_resp->set_status_code(std::to_string(status_code));
-	http_resp->append_output_body(data.dump().c_str());
+void json_response(protocol::HttpResponse *http_resp, json data,
+                   int status_code) {
+  http_resp->set_status_code(std::to_string(status_code));
+  http_resp->append_output_body(data.dump().c_str());
 
-	// can we reuse this?
-	protocol::HttpMessageHeader header;
-	header.name = "Content-Type";
-	header.value = "application/json";
+  // can we reuse this?
+  protocol::HttpMessageHeader header;
+  header.name = "Content-Type";
+  header.value = "application/json";
 
-	http_resp->set_header(&header);
+  http_resp->set_header(&header);
 }
 
 int main(void) {
-	WFHttpServer server([](WFHttpTask *task) {
-		std::string path = std::string(task->get_req()->get_request_uri());
+  WFHttpServer server([](WFHttpTask *task) {
+    std::string path = std::string(task->get_req()->get_request_uri());
 
-		std::smatch match;
-		if(!std::regex_match(path, match, kRpcRegex)) {
-			json_response(task->get_resp(), json({{"code", 404}, {"error", "Resource Not Found"}}), 404);
-			return;
-		}
-	});
+    std::smatch match;
+    if (!std::regex_match(path, match, kRpcRegex)) {
+      json_response(task->get_resp(),
+                    json({{"code", 404}, {"error", "Resource Not Found"}}),
+                    404);
+      return;
+    }
+  });
 
-	int error;
-	if ((error = server.start(8080)) == 0) {
-		// stops server when any key + enter is pressed
-		getchar();
-		server.stop();
-	} else {
-		fprintf(stderr, "error: code %d\n", error);
-	}
+  int error;
+  if ((error = server.start(8080)) == 0) {
+    // stops server when any key + enter is pressed
+    getchar();
+    server.stop();
+  } else {
+    fprintf(stderr, "error: code %d\n", error);
+  }
 
-	return 0;
+  return 0;
 }
--- src/rpc.h
+++ src/rpc.h
@@ -1,28 +1,21 @@
 #pragma once
 
-#include <string>
 #include <json.hpp>
+#include <string>
 
 using json = nlohmann::json;
 
-enum HTTPMethod {
-	POST,
-	PUT,
-	PATCH,
-	DELETE
-};
+enum HTTPMethod { POST, PUT, PATCH, DELETE };
 
 class RPC {
 
-	public:
-		std::string name;
-		HTTPMethod method;
+public:
+  std::string name;
+  HTTPMethod method;
 
-	public:
-		RPC(std::string name, HTTPMethod method) :
-			name(name), method(method) {}
+public:
+  RPC(std::string name, HTTPMethod method) : name(name), method(method) {}
 
-	public:
-		json execute(json arguments);
-
+public:
+  json execute(json arguments);
 };
